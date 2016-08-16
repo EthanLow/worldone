84a31a5c6999734e75386558c0474599f6006bb8
EthanLow <liuleiailin@sina.com>
2016-08-16 11:51:06 +0800

    首次上传
    首次上传

---
+++ Code/SVN/SVN/AppDelegate.m
@@ -0,0 +1,56 @@
+//
+//  AppDelegate.m
+//  SVN
+//
+//  Created by tarena on 16/8/16.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"宝宝不哭，站起来撸");
+    NSLog(@"djfhnkaldmfkladsf");
+    NSLog(@"asjdlskfmkladsmfkl");
+    NSLog(@"独孤求败");
+    NSLog(@"火云邪神大人");
+    NSLog(@"lalalalallala");
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"jdjsdad");
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+   
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+   
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ Code/SVN/SVN/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  SVN
+//
+//  Created by tarena on 16/8/16.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+       
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
